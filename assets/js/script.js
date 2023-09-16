bed290083a3ab9257898c317832d20e6686b19cd
Arthur Silva Dantas <arthur.dantas@estudante.ifms.edu.br>
2023-09-16 15:26:11 -0300

    creation header

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+const navbar = document.querySelector("[data-navbar]");
+const navbarToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
+
+navbarToggleBtn.addEventListener("click", function () { elemToggleFunc(navbar); });
